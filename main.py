1b493a059beea8fee3334d860e8a6c09730319fb
Tokhab <tokhab@gmail.com>
2021-12-15 22:10:08 +0300

    Added y_trans.py

--- main.py
+++ main.py
@@ -1,25 +1,77 @@
-from datetime import datetime as dt
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
 
-def timeit(func):
-    def wrapper(*args, **kwargs):
-        start = dt.now()
-        result = func(*args, **kwargs)
-        print(f'Function "{func.__name__}" has worked for {dt.now() - start}')
-        return result
-    return wrapper
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
 
-@timeit
-def one(n):
-    arr = []
-    for i in range(n):
-        if i %2 == 0:
-            arr.append(i)
-    return arr
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
 
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
-if __name__ == '__main__':
-    print(one(10000))
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+# Encoding the Dependent Variable
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
+print(X_train)
+print(X_test)
+
+# from datetime import datetime as dt
+#
+#
+# def timeit(func):
+#     def wrapper(*args, **kwargs):
+#         start = dt.now()
+#         result = func(*args, **kwargs)
+#         print(f'Function "{func.__name__}" has worked for {dt.now() - start}')
+#         return result
+#     return wrapper
+#
+# @timeit
+# def one(n):
+#     arr = []
+#     for i in range(n):
+#         if i %2 == 0:
+#             arr.append(i)
+#     return arr
+#
+#
+# if __name__ == '__main__':
+#     print(one(10000))
 
 
 # import re
---
+++ y_trans.py
@@ -0,0 +1,3 @@
+a = 'https://dictionary.yandex.net/dicservice.json/lookupMultiple?sid=b5c22f4a.61ba3be7.322aa344.74722d74657874&' \
+    'ui=ru&srv=tr-text&text=inquiries&type=regular%2Csyn%2Cant%2Cderiv&lang=en-ru&flags=7591&' \
+    'dict=en-ru.regular%2Cen.syn%2Cen.ant%2Cen.deriv&yu=6615621741616725357&yum=1616725360738406997'
